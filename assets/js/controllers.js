b31c174b9085e1d9b71041eb4cdea557b8aa4968
Trevan Hetzel <trevan@hetzelcreative.com>
2014-06-09 17:53:37 -0500

    Base dashboard, projects and public paths

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,9 +3,10 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('Project', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+
+  .controller('Dashboard', ['$scope', function($scope) {
 
   }]);

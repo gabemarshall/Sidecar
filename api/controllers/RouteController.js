8eda91c83f4aadd084ac401030d2971ffb5b6b20
Trevan Hetzel <trevan@hetzelcreative.com>
2014-06-13 16:19:15 -0500

    Trash mean.io

--- api/controllers/RouteController.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-	index: function (req, res) {
-		res.view('app/index', { layout: 'layout-app' });
-	},
-
-	_config: {}
-  
-};
--- server/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// User routes use users controller
-var dashboard = require('../controllers/dashboard');
-
-module.exports = function(app, passport) {
-	
-};

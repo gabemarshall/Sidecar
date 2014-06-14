c9d0e5004d9242680724b5ae625284ccc9c01c80
Jon Bellah <jbellah@gmail.com>
2014-06-13 19:09:07 -0500

    Tabs now work on the Tasks view

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -22,6 +22,9 @@ angular.module('sidecar.controllers', [])
   		$scope.toggleModal = function() {
     		$scope.modalShown = !$scope.modalShown;
   		};
+
+  		// Set default tab to the project overview
+  		$scope.selected=1;
 	}])
 	.controller('Dashboard', ['$scope', function ($scope) {
 

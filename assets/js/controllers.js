95d33e86c96b32ba152f7f934272765af27a21ca
Gabe Marshall <gabemarshall@me.com>
2014-06-23 17:04:36 -0500

    Completed initial backend-to-angular of projects and tasks

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -5,12 +5,41 @@ angular.module('sidecar.controllers', [])
 	.controller('Nav', ['$scope', '$location', function ($scope, $location) {
 		$scope.$location = $location;
 	}])
-	.controller('Project', ['$scope', function ($scope) {
+	.controller('Project', ['$scope', '$http', function ($scope, $http) {
 		// Modal controller
 		$scope.modalShown = false;
   		$scope.toggleModal = function() {
     		$scope.modalShown = !$scope.modalShown;
   		};
+    $scope.projects = []
+    $scope.newProjectTitle = '';
+
+    var ajaxGetProjects = function(){
+      $http({
+        method: 'GET',
+        url: '/projects'
+      })
+      .success(function (data, status, headers, config){
+        $scope.projects = data
+      })
+    }
+    ajaxGetProjects()
+
+    $scope.saveNewProject = function(value){
+      var title = this.newProjectTitle
+
+      $http({
+          method: "post",
+          url: "/projects/create",
+          data: {
+              title: title
+          }
+      })
+      .success(function(){
+        ajaxGetProjects()
+      })
+
+    }
 	}])
   .controller('Clients', ['$scope', function ($scope) {
     // Modal controller
@@ -21,8 +50,9 @@ angular.module('sidecar.controllers', [])
   }])
 	.controller('Tasks', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
 		$('.dial').knob();
+    
     var title = $routeParams.title
-    console.log(title)
+  
     $scope.newTaskTitle = '';
     $scope.project = []
 

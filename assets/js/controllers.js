b88539a17558e4fa457a21e96ae86659e089dccf
Gabe Marshall <gabemarshall@me.com>
2014-07-08 16:10:44 -0500

    Reverted to eef85d135d3309ce5dae00dce45e744ae2e8e56b , tried to re-implement most of Jons changes..

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -298,4 +298,25 @@ angular.module('sidecar.controllers', [])
       $scope.editToggleModal = function() {
         $scope.editModalShown = !$scope.editModalShown;
       };
-  }]);
+  }])
+
+.controller('Documents', ['$scope', function($scope) {
+    $scope.documentTemplates = [{
+      name: 'Overview',
+      url: 'partials/document/overview.html'
+    }, {
+      name: 'Single',
+      url: 'partials/document/single.html'
+    }];
+
+    $scope.documentTemplate = $scope.documentTemplates[0];
+
+    $scope.loadOverview = function(){
+      $scope.documentTemplate = $scope.documentTemplates[0];
+    }
+
+    $scope.loadSingle = function() {
+      $scope.documentTemplate = $scope.documentTemplates[1];
+    }
+
+  }])

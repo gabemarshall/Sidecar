012d90306cf903c73e731a29c5c5f1ba8ad9652e
Jon Bellah <jbellah@gmail.com>
2014-07-28 19:44:53 -0500

    Some UI exploration

--- assets/js/directives.js
+++ assets/js/directives.js
@@ -3,129 +3,133 @@
 /* Directives */
 
 angular.module('sidecar.directives', []).
-	directive('appVersion', ['version', function(version) {
+directive('appVersion', ['version',
+	function(version) {
 		return function(scope, elm, attrs) {
 			elm.text(version);
 		};
-	}])
+	}
+])
 	.directive('modalDialog', function() {
-	  return {
-	    restrict: 'E',
-	    scope: {
-	      show: '='
-	    },
-	    replace: true, // Replace with the template below
-	    transclude: true, // we want to insert custom content inside the directive
-	    link: function(scope, element, attrs) {
-	      scope.dialogStyle = {};
-	      if (attrs.width)
-	        scope.dialogStyle.width = attrs.width;
-	      if (attrs.height)
-	        scope.dialogStyle.height = attrs.height;
-	      scope.hideModal = function() {
-	        scope.show = false;
-	      };
-	    },
-	    templateUrl: 'partials/modal.html' // See below
-	  };
-	})
-
-	.directive('subNav', function () {
-		return {
-			restrict: 'A',
-
-			link: function (scope, element) {
-				var $anchor = element.find('a');
-
-				$anchor.on('click', function () {
-					var $this = $(this);
-
-					$anchor.removeClass('selected');
-					$this.addClass('selected');
-				})
-			}
-		}
-	})
-	.directive('dial', function () {
 		return {
 			restrict: 'E',
-		    scope: {
-		      dialValue: '='
-		    },
-			template: '<input type="text" value="{{dialValue}}" data-width="180" data-thickness="0.125" \
-			data-fgColor="#428bca" data-readOnly=true class="dial">',
-			link: function (scope, element) {
+			scope: {
+				show: '='
+			},
+			replace: true, // Replace with the template below
+			transclude: true, // we want to insert custom content inside the directive
+			link: function(scope, element, attrs) {
+				scope.dialogStyle = {};
+				if (attrs.width)
+					scope.dialogStyle.width = attrs.width;
+				if (attrs.height)
+					scope.dialogStyle.height = attrs.height;
+				scope.hideModal = function() {
+					scope.show = false;
+				};
+			},
+			templateUrl: 'partials/modal.html' // See below
+		};
+	})
+
+.directive('subNav', function() {
+	return {
+		restrict: 'A',
+
+		link: function(scope, element) {
+			var $anchor = element.find('a');
+
+			$anchor.on('click', function() {
+				var $this = $(this);
+
+				$anchor.removeClass('selected');
+				$this.addClass('selected');
+			})
+		}
+	}
+})
+	.directive('dial', function() {
+		return {
+			restrict: 'E',
+			scope: {
+				dialValue: '='
+			},
+			template: '<input type="text" value="{{dialValue}}" data-width="180" data-thickness="0.2" \
+			data-fgColor="#23b1d1" data-readOnly=true class="dial">',
+			link: function(scope, element) {
 				var input = element.find("input")[0];
-				setTimeout(function(){
-					$(input).knob()
-				},0);
+				setTimeout(function() {
+					$(input).knob({
+						'format': function(v) {
+							return v + '%'
+						}
+					})
+				}, 0);
 			}
 		}
 	})
-    .directive("clickToEdit", function($http, $timeout) {
-    var editorTemplate = '<section class="click-to-edit">' +
-            '<input type="checkbox" data-status="{{status}}" name="vehicle" value="Car"><span ng-hide="view.editorEnabled" ng-click="enableEditor()">{{value}}</span>' +
-            '<input ng-keypress="watchKeys($event)" ng-show="view.editorEnabled" ng-model="view.editableValue" ng-blur="save()">' +
-    '</section>';
+	.directive("clickToEdit", function($http, $timeout) {
+		var editorTemplate = '<section class="click-to-edit">' +
+			'<input type="checkbox" data-status="{{status}}" name="vehicle" value="Car"><span ng-hide="view.editorEnabled" ng-click="enableEditor()">{{value}}</span>' +
+			'<input ng-keypress="watchKeys($event)" ng-show="view.editorEnabled" ng-model="view.editableValue" ng-blur="save()">' +
+			'</section>';
 
-    return {
-        restrict: "A",
-        replace: true,
-        template: editorTemplate,
-        scope: {
-            value: "=clickToEdit",
-            attr: "=taskId",
-            status: "=statusBool"
-        },
-        link: function(scope, element, attrs) {
-            element.bind('click', function() {
-                $timeout(function() {
-                    element.find('input')[1].focus();
-                });
-            });
+		return {
+			restrict: "A",
+			replace: true,
+			template: editorTemplate,
+			scope: {
+				value: "=clickToEdit",
+				attr: "=taskId",
+				status: "=statusBool"
+			},
+			link: function(scope, element, attrs) {
+				element.bind('click', function() {
+					$timeout(function() {
+						element.find('input')[1].focus();
+					});
+				});
 
-        },
-        controller: function($scope) {
-            $scope.view = {
-                editableValue: $scope.value,
-                editorEnabled: false
-            };
+			},
+			controller: function($scope) {
+				$scope.view = {
+					editableValue: $scope.value,
+					editorEnabled: false
+				};
 
-            $scope.watchKeys = function(event){
-            	if (event.which == 13){
-            		$scope.disableEditor()
-            	}
-            }
+				$scope.watchKeys = function(event) {
+					if (event.which == 13) {
+						$scope.disableEditor()
+					}
+				}
 
-            $scope.enableEditor = function() {
-                $scope.view.editorEnabled = true;
-                $scope.view.editableValue = $scope.value;
-            };
+				$scope.enableEditor = function() {
+					$scope.view.editorEnabled = true;
+					$scope.view.editableValue = $scope.value;
+				};
 
-            $scope.disableEditor = function() {
-                $scope.view.editorEnabled = false;
-            };
+				$scope.disableEditor = function() {
+					$scope.view.editorEnabled = false;
+				};
 
-            $scope.save = function() {
-                $scope.value = $scope.view.editableValue;
-                $scope.disableEditor();
-                $scope.updateTask();
-            };
-	        $scope.updateTask = function (index) {
-	          $http({
-	              method: "post",
-	              url: "/tasks/update/" + $scope.attr,
-	              data: {
-	                  title: $scope.value
-	              }
-	          })
-	          .success(function(){
-	            console.log("Task updated")
+				$scope.save = function() {
+					$scope.value = $scope.view.editableValue;
+					$scope.disableEditor();
+					$scope.updateTask();
+				};
+				$scope.updateTask = function(index) {
+					$http({
+						method: "post",
+						url: "/tasks/update/" + $scope.attr,
+						data: {
+							title: $scope.value
+						}
+					})
+						.success(function() {
+							console.log("Task updated")
 
-	          })
-	        };
-        }
-    };
-});
-
-
+						})
+				};
+			}
+		};
+	});

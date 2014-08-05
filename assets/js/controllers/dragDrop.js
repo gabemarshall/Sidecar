0b607fb271729647ab4741ee37d57c3ba750c385
Gabe Marshall <gabemarshall@me.com>
2014-08-05 10:20:45 -0500

    Initial work on drag drop front to backend functionality

---
+++ assets/js/controllers/dragDrop.js
@@ -0,0 +1,9 @@
+// DragDrop
+// ************************************************************************
+// angular.module('sidecar.controllers').controller('DragDropCtrl', function($scope) {
+//     $scope.handleDrop = function() {
+//         alert('Item has been dropped');
+//     }
+// });
+
+
